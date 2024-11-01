aaacf46198bddd25601a0d754e6c0f9cdc82fbc5
Agape Ioan <ioanagape14@gmail.com>
2024-11-01 20:40:21 +0200

    Passed the program name via args
    Program B will launch Program A as a separate process, **provided as an argument**.

--- program_B.py
+++ program_B.py
@@ -3,6 +3,7 @@ import threading
 import queue
 import sys
 import os
+import argparse
 
 STDOUT_TIMEOUT = 1
 DEBUG = False
@@ -67,9 +68,11 @@ class Controller:
         return self.get_response()
     
 def main():
-    controller = Controller(RNG_PROGRAM_NAME)
-    # controller = Controller('program_a.kt')
-    # controller = Controller('program_a.jar')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('program_name', type=str, help='The program to run (e.g., program_a.py, program_a.kt, program_a.jar)')
+    args = parser.parse_args()
+
+    controller = Controller(args.program_name)
     
     random_numbers = []
     
@@ -145,35 +148,6 @@ def debug():
         if command == 'Shutdown':
             print("Shutting down.")
             break
-
-def _test():
-    rng_proc = subprocess.Popen(['python', 'program_a.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
-
-    output_queue = queue.Queue()
-    threading.Thread(target=read_stdout, args=(rng_proc, output_queue), daemon=True).start()
-
-    try:
-        while True:
-            message_to_b = input("Enter a command: ")
-
-            rng_proc.stdin.write(message_to_b + '\n')
-            rng_proc.stdin.flush()
-
-            try:
-                response_from_b = output_queue.get(timeout=STDOUT_TIMEOUT).strip()
-                print(f"Response:\n{response_from_b}")
-            except queue.Empty:
-                # print("Timeout. No response from program A.")
-                continue # ignore
-
-            if message_to_b.lower() == 'shutdown':
-                print("Shutting down.")
-                break
-
-    finally:
-        # Close communication
-        rng_proc.stdin.close()
-        rng_proc.wait()
 
 if __name__ == "__main__":
     if DEBUG:
